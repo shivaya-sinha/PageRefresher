3828d61199c03fa19d6e239819eeca6d3c56f1a8
Shilpi <shilpi.c75@gmail.com>
2016-10-17 22:57:54 +0530

    Corrected Logic and errors
    Code is corrected for logic and errors.

--- Refresher.py
+++ Refresher.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+import time
 x=raw_input("Enter the URL")
 refreshrate=raw_input("Enter the number of seconds")
 refreshrate=int(refreshrate)
 driver = webdriver.Firefox()
 driver.get("http://"+x)
 while True:
+	time.sleep(refreshrate)
 	driver.refresh()
